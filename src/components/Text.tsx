7596f395bd84e98c29ab3d6f3ac179c5c27a5ae8
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-10-17 20:53:52 -0300

    Create Text.tsx

---
+++ src/components/Text.tsx
@@ -0,0 +1,19 @@
+import { clsx } from "clsx";
+
+interface TextProps {
+    size?: "sm" | "md" | "lg"
+}
+
+function Text({ size = "md" }: TextProps) {
+    return (
+        <span className={clsx("text-gray-100 font-sans", {
+            'text-xs': size === "sm",
+            'text-sm': size === "md",
+            "text-md": size === "lg",
+        })}>
+            Text
+        </span>
+    )
+}
+
+export { Text };
